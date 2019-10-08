8d72bc9e1c3fafd0b39f1d1ffe842dd5aa455951
Mei Wong <fongmeiw@gmail.com>
2019-10-08 19:31:06 +0000

    Automatically backed up by Learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -7,7 +7,7 @@ def array_with_two_elements()
 end
 
 def first_element(array)
-  array.first()
+  array[0]
 end
 
 def third_element(array)
@@ -15,17 +15,17 @@ def third_element(array)
 end
 
 def last_element(array)
+  array[-1]
+end
+
+def first_element_with_array_methods(array)
+  array.first()
+end
+
+def last_elemnt_with_array_methods(array)
   array.last()
 end
 
-def first_element_with_array_methods()
-  
-end
-
-def last_elemnt_with_array_methods()
-  
-end
-
-def length_of_array()
-  
+def length_of_array(array)
+  array.size
 end
